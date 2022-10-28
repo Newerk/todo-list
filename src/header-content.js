146df5d4296d1d8af5ceb6920d72cb744cb11333
newerk <juniusowens@yahoo.com>
2022-10-28 13:15:23 -0400

    Add Placeholder Content for Page Header
    For now, Im using the textContent dot notation to help me get and idea
    of how the content will be placed in the header. This will eventually
    change to using other modules to proper determine what type of content
    is in the header besides text.

--- src/header-content.js
+++ src/header-content.js
@@ -1,1 +1,31 @@
-//will appropriately place header content in the grid of the header
+//Module will appropriately place header content into the display grid layout
+
+//Retrieve the header div to manipulate content
+const headerDiv = document.querySelector('.header')
+
+//the dot notation 'textContent' are placeholder values for the content that will be assigned to the page header
+export const headerContent = () => {
+    const headerLogo = (() => {
+        const logo = document.createElement('div');
+        logo.id = 'header-logo';
+        logo.textContent = 'LOGO';
+        logo.setAttribute('style', 'grid-column: 1 / 2; color: white');
+        headerDiv.appendChild(logo);
+    })();
+
+    const headerUserName = (() => {
+        const userName = document.createElement('div');
+        userName.id = 'header-username';
+        userName.textContent = 'USERNAME---';
+        userName.setAttribute('style', 'grid-column: 3 / 4; color: white');
+        headerDiv.appendChild(userName);
+    })();
+
+    const headerProfileImg = (() => {
+        const pfp = document.createElement('div');
+        pfp.id = 'header-pfp';
+        pfp.textContent = 'PFP';
+        pfp.setAttribute('style', 'grid-column: 4 / 5; color: white');
+        headerDiv.appendChild(pfp);
+    })();
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-// import './body-grid-layout.css';
-
+import {headerContent} from './header-content.js'
 
 const content = document.getElementById('content');
 // content.textContent = "'TEST'";
 
 document.body.appendChild(content);
+
+const headerDiv = document.querySelector('.header');
+headerContent();
