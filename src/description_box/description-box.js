a6e23d6cbef01e1471117a734ee7bc1202d47bce
newerk <juniusowens@yahoo.com>
2022-11-01 13:09:51 -0400

    Add Skeleton DOM for Description Box
    The description box that will appear on hover will be split into 3 sections vertically. As of now, only the top section has its placeholder elements

--- src/description_box/description-box.js
+++ src/description_box/description-box.js
@@ -1,6 +1,54 @@
 import { content } from "..";
+import './description-box.css'
 
-const buildDescriptionBox = () => {
+export const buildDescriptionBox = () => {
     const descriptionContainer = document.createElement('div');
+    descriptionContainer.className = 'description-box-container';
+
+    const topContainer = document.createElement('div');
+    topContainer.className = 'top-container';
+
+    const priority = document.createElement('p');
+    priority.id = 'priority';
+    priority.textContent = 'Priority:';
+
+    const priorityValue = document.createElement('p');
+    priorityValue.id = 'priority-value';
+    priorityValue.textContent = 'USE JS TO FILL';
+
+    const status = document.createElement('p');
+    status.id = 'status';
+    status.textContent = 'Status:';
+
+    const statusValue = document.createElement('p');
+    statusValue.id = 'status-value';
+    statusValue.textContent = 'USE JS TO FILL';
+
+    const dateCreation = document.createElement('p');
+    dateCreation.id = 'date-creation';
+    dateCreation.textContent = 'Date Created:';
+
+    const dateCreationValue = document.createElement('p');
+    dateCreationValue.id = 'date-creation-value';
+    dateCreationValue.textContent = 'USE JS TO FILL';
+
+    topContainer.appendChild(priority)
+    topContainer.appendChild(priorityValue)
+    topContainer.appendChild(status)
+    topContainer.appendChild(statusValue)
+    topContainer.appendChild(dateCreation)
+    topContainer.appendChild(dateCreationValue)
+
+
+    const middleContainer = document.createElement('div');
+
+
+    const bottomContainer = document.createElement('div');
+
+    
+
+    descriptionContainer.appendChild(topContainer)
+
+    content.appendChild(descriptionContainer);
 
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { headerContent } from './header/header-content.js';
 import { compactSidebarContent } from './sidebar/compact_sidebar/compact-sidebar-content.js';
 import './style.css'
 
+import { buildDescriptionBox } from './description_box/description-box.js';
+
 
 export const content = document.createElement('div');
 content.id = 'content';
@@ -9,4 +11,5 @@ document.body.appendChild(content);
 
 headerContent();
 compactSidebarContent();
+buildDescriptionBox();
 
