fa710e5feef089293cd5268a9a466e888500d1f6
newerk <juniusowens@yahoo.com>
2022-11-01 14:41:24 -0400

    Stop Description Box From Showing in Content Space

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { headerContent } from './header/header-content.js';
 import { compactSidebarContent } from './sidebar/compact_sidebar/compact-sidebar-content.js';
 import './style.css'
 
-import { buildDescriptionBox } from './description_box/description-box.js';
 
 
 export const content = document.createElement('div');
@@ -11,5 +10,4 @@ document.body.appendChild(content);
 
 headerContent();
 compactSidebarContent();
-buildDescriptionBox();
 
