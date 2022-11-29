a3eb4ad89e3a64b3540f306fb0c4c4e8ca3dda12
newerk <juniusowens@yahoo.com>
2022-11-29 15:30:49 -0500

    Make Filters Apply Correctly No Matter The Year
    Fixed the issue with dates set in a different year would cause todos to appear on the wrong page

--- src/todoFilterLogic.js
+++ src/todoFilterLogic.js
@@ -1,14 +1,19 @@
 export const todoFilterLogic = (obj) => {
     let todaysDate = new Date;
-    let formattedDate = `${todaysDate.getMonth()+1}/${todaysDate.getDate()}/${todaysDate.getFullYear()}`;
+    // let formattedDate = `${todaysDate.getMonth()+1}/${todaysDate.getDate()}/${todaysDate.getFullYear()}`;
+    let formattedTodaysDate = `${todaysDate.getFullYear()}-${todaysDate.getMonth()+1}-${todaysDate.getDate()}`;
 
-    if (obj.dueDate === formattedDate) {
+
+    let breakApart = obj.dueDate.split('/');
+    let formattedDueDate = `${breakApart[2]}-${breakApart[0]}-${breakApart[1]}`
+
+    if (formattedDueDate === formattedTodaysDate) {
         obj.filter = 'today';
 
-    } else if (obj.dueDate > formattedDate) {
+    } else if (formattedDueDate > formattedTodaysDate) {
         obj.filter = 'upcoming';
 
-    } else if (obj.dueDate < formattedDate) {
+    } else if (formattedDueDate < formattedTodaysDate) {
         obj.filter = 'pastdue';
 
     }
