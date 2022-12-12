6601771d5d7a73b49f483e811f6b64caf7d9b85b
newerk <juniusowens@yahoo.com>
2022-12-12 17:17:51 -0500

    Split Card Container Into 3 Parts W/ Grid

--- src/projects-card.js
+++ src/projects-card.js
@@ -6,9 +6,17 @@ export const projectsCard = () => {
     const cardContainer = document.createElement('div');
     cardContainer.className = 'projects-card-container'
 
-    const topContainer = '';
-    const middleContainer = '';
-    const bottomContainer = '';
+
+    const topContainer = document.createElement('div');
+    topContainer.id = 'card-top-container';
+
+    const middleContainer = document.createElement('div');;
+    middleContainer.id = 'card-middle-container';
+
+    const bottomContainer = document.createElement('div');;
+    bottomContainer.id = 'card-bottom-container';
+
+    cardContainer.append(topContainer, middleContainer, bottomContainer)
 
     return cardContainer;
 
