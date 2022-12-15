c3c067418b5a4f712284c35d3145fac9974c8bc4
newerk <juniusowens@yahoo.com>
2022-12-15 14:39:06 -0500

    Add Variables to Structure Expanded Project Menu

--- src/selected-project-popout.js
+++ src/selected-project-popout.js
@@ -3,5 +3,30 @@ export const projectPopOut = () => {
     container.className = 'project-popout-container';
     container.setAttribute('style', 'position: absolute; width: 60vw; height: 80.7%; background-color: orange; z-index: 5; place-self: center')
 
+    const top = '';
+        const title = '';
+        const exitBtn = '';
+
+    top.append(title, exitBtn);
+
+    const middle = '';
+        const descriptionWrapper = '';
+            const descriptionTag = '';
+            const description = '';
+        const tasksWrapper = '';
+            const taskheader = '';
+                const tasksTag = '';
+                const newTaskBtn = '';
+            const taskContainer = '';
+
+    middle.append(descriptionWrapper,tasksWrapper);
+
+    const bottom = '';
+        const editBtn = '';
+
+    bottom.append(editBtn);
+
+    container.append(top,middle,bottom);
+
     return container;
 }
