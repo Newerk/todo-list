6b2df86eb72364f68ce979eb1688c796b19aef7a
newerk <juniusowens@yahoo.com>
2023-01-04 10:39:17 -0500

    Add IDs and Class to DOM Elements

--- src/pages/account.js
+++ src/pages/account.js
@@ -10,18 +10,31 @@ export const accountPage = () => {
     accountContent.setAttribute('style', 'display: grid; grid-template-rows: 1fr 1fr; width: 100%; height: 100%;');
 
     const top = document.createElement('div');
-    top.setAttribute('style', 'background-color: blue; width: 100%; height: 100%; grid-row: 1')
+    top.setAttribute('style', 'background-color: blue; width: 100%; height: 100%; grid-row: 1; display: grid; grid-template-rows: auto 1fr')
 
     const topRowOneWrapper = document.createElement('div');
+    topRowOneWrapper.id = 'top-row-one-wrapper';
     const profilePic = document.createElement('div');
+    profilePic.id = 'ap-profile-pic';
     const profilePicEditBtn = document.createElement('div');
+    profilePicEditBtn.className = 'ap-edit-btn';
+    profilePicEditBtn.id = 'profile-edit-btn';
+
+    topRowOneWrapper.append(profilePic, profilePicEditBtn);
+
 
 
     const topRowTwoWrapper = document.createElement('div');
+    topRowTwoWrapper.id = 'top-row-two-wrapper';
     const hello = document.createElement('div');
     hello.textContent = 'Hello, ';
     const username = document.createElement('div');
     const usernameEditBtn = document.createElement('div');
+
+    topRowTwoWrapper.append(hello, username, usernameEditBtn);
+
+    top.append(topRowOneWrapper, topRowTwoWrapper);
+
 
 
 
