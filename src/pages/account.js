afb51d4cc71e09af44dbc2547989078243ae1c1c
newerk <juniusowens@yahoo.com>
2023-01-04 13:27:20 -0500

    Change Cursor On Every Button Once Hovered

--- src/pages/account.js
+++ src/pages/account.js
@@ -16,7 +16,7 @@ export const accountPage = () => {
     topRowOneWrapper.id = 'top-row-one-wrapper';
     const profilePic = document.createElement('div');
     profilePic.id = 'ap-profile-pic';
-    const profilePicEditBtn = document.createElement('div');
+    const profilePicEditBtn = document.createElement('button');
     profilePicEditBtn.className = 'ap-edit-btn';
     profilePicEditBtn.id = 'profile-edit-btn';
 
@@ -29,10 +29,11 @@ export const accountPage = () => {
     const topRowTwoWrapper = document.createElement('div');
     topRowTwoWrapper.id = 'top-row-two-wrapper';
     const hello = document.createElement('div');
+    hello.id = 'hello';
     hello.textContent = 'Hello, ';
     const username = document.createElement('div');
     username.textContent = '______________________'
-    const usernameEditBtn = document.createElement('div');
+    const usernameEditBtn = document.createElement('button');
     usernameEditBtn.className = 'ap-edit-btn';
     usernameEditBtn.id = 'username-edit-btn';
 
@@ -59,11 +60,13 @@ export const accountPage = () => {
 
     profilePicCollection.id = 'profile-pic-collection';
 
+
     const picsArr = []//directories to different pfp images. I will interate through this in the for loop below
 
+    //will be updated to: for (let i = 0; i < picsArr.length; i++) 
     for (let i = 0; i < 15; i++) {
         const test = document.createElement('div');
-        test.setAttribute('style', 'height: 5rem; width: 5rem; background-color: orange; border-radius: 50%');
+        test.setAttribute('style', 'height: 5rem; width: 5rem; background-color: orange; border-radius: 50%; cursor: pointer;');
         test.addEventListener('click', () => {
             //apply selected picture to profile and header
             console.log('click!');
