045810552f5e7a67a418f4fc5bd7c4414b9e1061
newerk <juniusowens@yahoo.com>
2023-01-08 16:24:45 -0500

    Fix The Highlighting Of Current Page in Sidebar

--- src/sidebar/sidebar-active-page-colors.js
+++ src/sidebar/sidebar-active-page-colors.js
@@ -3,47 +3,61 @@ import { storageManagement } from "../local-storage";
 export const changeBtnBg = () => {
     if (storageManagement.sideBarIsExpanded.value === true) {
 
+        if (storageManagement.onHomePage.value === true) {
+            document.querySelector('#home-btn-ex').setAttribute('style', 'background-color: #37373C');
 
-        switch (true) {
-            case storageManagement.onHomePage.value:
-                document.querySelector('#home-btn-ex').setAttribute('style', 'background-color: #37373C');
+        } else {
+            document.querySelector('#home-btn-ex').setAttribute('style', 'background-color: #252526');
 
-                break;
-
-            case storageManagement.onTodayPage.value:
-                document.querySelector('#today-btn-ex').setAttribute('style', 'background-color: #37373C');
-
-                break;
-
-            case storageManagement.onUpcomingPage.value:
-                document.querySelector('#upcoming-btn-ex').setAttribute('style', 'background-color: #37373C');
-
-                break;
-
-            case storageManagement.onPastDuePage.value:
-                document.querySelector('#pastdue-btn-ex').setAttribute('style', 'background-color: #37373C');
-
-                break;
-
-            case storageManagement.onProjectsPage.value:
-                document.querySelector('#projects-btn-ex').setAttribute('style', 'background-color: #37373C');
-
-                break;
-
-            case storageManagement.onAccountPage.value:
-                document.querySelector('#account-btn-ex').setAttribute('style', 'background-color: #37373C');
-
-                break;
-
-            case storageManagement.onSettingsPage.value:
-                document.querySelector('#settings-btn-ex').setAttribute('style', 'background-color: #37373C');
-
-                break;
-
-            default:
-
-        //    .setAttribute('style', 'background-color: #252526')
-                break;
         }
+
+        if (storageManagement.onTodayPage.value === true) {
+            document.querySelector('#today-btn-ex').setAttribute('style', 'background-color: #37373C');
+
+        } else {
+            document.querySelector('#today-btn-ex').setAttribute('style', 'background-color: #252526');
+
+        }
+
+        if (storageManagement.onUpcomingPage.value === true) {
+            document.querySelector('#upcoming-btn-ex').setAttribute('style', 'background-color: #37373C');
+
+        } else {
+            document.querySelector('#upcoming-btn-ex').setAttribute('style', 'background-color: #252526');
+
+        }
+
+        if (storageManagement.onPastDuePage.value === true) {
+            document.querySelector('#pastdue-btn-ex').setAttribute('style', 'background-color: #37373C');
+
+        } else {
+            document.querySelector('#pastdue-btn-ex').setAttribute('style', 'background-color: #252526');
+
+        }
+
+        if (storageManagement.onProjectsPage.value === true) {
+            document.querySelector('#projects-btn-ex').setAttribute('style', 'background-color: #37373C');
+
+        } else {
+            document.querySelector('#projects-btn-ex').setAttribute('style', 'background-color: #252526');
+
+        }
+
+        if (storageManagement.onAccountPage.value === true) {
+            document.querySelector('#account-btn-ex').setAttribute('style', 'background-color: #37373C');
+
+        } else {
+            document.querySelector('#account-btn-ex').setAttribute('style', 'background-color: #252526');
+
+        }
+
+        if (storageManagement.onSettingsPage.value === true) {
+            document.querySelector('#settings-btn-ex').setAttribute('style', 'background-color: #37373C');
+
+        } else {
+            document.querySelector('#settings-btn-ex').setAttribute('style', 'background-color: #252526');
+
+        }
+
     }
 }
