dcd2e10c7cfdfdbd10a8344035be292361d5edac
newerk <juniusowens@yahoo.com>
2023-01-11 11:41:07 -0500

    Remove More Console.logs

--- src/todoFilterLogic.js
+++ src/todoFilterLogic.js
@@ -1,3 +1,5 @@
+import { updateScreenTasksLS } from "./update-info";
+
 export const todoFilterLogic = (obj) => {
     let todaysDate = new Date;
     // let formattedTodaysDate = `${todaysDate.getMonth()+1}/${todaysDate.getDate()}/${todaysDate.getFullYear()}`;
@@ -6,30 +8,23 @@ export const todoFilterLogic = (obj) => {
     let day = todaysDate.getDate();
 
     if (day < 10) {
-        console.log('day is less than 10')
 
         day = '0' + day;
-        console.log(day)
         
     }
 
     if (month < 10) {
-        console.log('month is less than 10')
 
         month = '0' + month;
-        console.log(month)
         
     }
     
     
     let formattedTodaysDate = `${year}-${month}-${day}`;
-    console.log('formatted todays date is: ' + formattedTodaysDate);
-
 
     let breakApart = obj.dueDate.split('/');
 
     let formattedDueDate = `${breakApart[2]}-${breakApart[0]}-${breakApart[1]}`;
-    // let formattedDueDate = `${parseInt(breakApart[0], 10)}/${parseInt(breakApart[1], 10)}/${breakApart[2]}`
 
 
     if (formattedDueDate === formattedTodaysDate) {
@@ -38,22 +33,12 @@ export const todoFilterLogic = (obj) => {
     } else if (formattedDueDate > formattedTodaysDate) {
         obj.filter = 'upcoming';
 
-        console.log('IT WORKS!!!!');
-
 
     } else if (formattedDueDate < formattedTodaysDate) {
         obj.filter = 'pastdue';
 
-
     }
 
-    console.log('formatted due date is: ' + formattedDueDate);
+    updateScreenTasksLS();
 
-
-
-}
-
-function MyInt(s){
-    this.asString = s;
-    this.num = parseInt(s);
 }
